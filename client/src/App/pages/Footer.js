a5c9e96bb9d0e49290a220b07c734ba1c1016eee
ubryce <bknguyen@ualberta.ca>
2020-03-07 19:00:13 -0700

    footer ex

--- client/src/App/pages/Footer.js
+++ client/src/App/pages/Footer.js
@@ -1,9 +1,14 @@
 import React from 'react'
 
 function Footer() {
+    
     return (
         <div>
-            Hello.
+        <footer className="footer text-center">
+            <div className="container">
+                <p className="footer-text">Copyright 2020 Blockchain. All rights reserved</p>
+            </div>
+        </footer>
         </div>
     )
 }
