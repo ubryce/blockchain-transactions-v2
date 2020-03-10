4467848fafca126aa2df6e88dcd7b7306842741d
ubryce <bknguyen@ualberta.ca>
2020-03-09 21:50:17 -0600

    cleaning up some code + changing display pic

--- client/src/App/pages/Display.js
+++ client/src/App/pages/Display.js
@@ -3,7 +3,7 @@ import React from 'react'
 function Display() {
     return (
         <div className="display">
-            <img src={ require('./Blockchain-Funds.png') }></img>
+            <img src={ require('./p1.gif') }></img>
         </div>
     )
 }
--- client/src/App/pages/Ethereum.js
+++ client/src/App/pages/Ethereum.js
@@ -1,6 +1,11 @@
 import React from 'react'
 
 function Ethereum() {
+
+    
+
+
+
     return (
         <div>
             <h1>Ethereum</h1>
