e6ac828b4b3561e7e63d1cf94371c70ba573619c
ubryce <bknguyen@ualberta.ca>
2020-03-10 18:20:57 -0600

    display transactions

--- client/src/App/pages/Transactions.js
+++ client/src/App/pages/Transactions.js
@@ -17,6 +17,21 @@ class Transactions extends Component{
         //console.log(data);
     }
 
+    renderTableData() {
+        return this.state.students.map((student, index) => {
+           const { id, name, age, email } = student //destructuring
+           return (
+              <tr key={id}>
+                 <td>{id}</td>
+                 <td>{name}</td>
+                 <td>{age}</td>
+                 <td>{email}</td>
+              </tr>
+           )
+        })
+     }
+
+
     render(){
 
         if(!this.state.transaction){
@@ -25,16 +40,39 @@ class Transactions extends Component{
         //console.log(this.state.transaction);
         const transactionsItems = this.state.transaction.map((transaction) =>
             <div>
-            <li key={transaction.blockchain}>{transaction.blockchain} {transaction.amount_usd} {transaction.from.owner} {transaction.to.owner}</li>
+            <li key={transaction.blockchain}>{transaction.blockchain} {transaction.amount} {transaction.amount_usd} {transaction.from.owner} {transaction.to.owner}</li>
             </div>
         );
 
         return (
             <div>
+                <h1 id="title">Recent Blockchain Transactions</h1>
                 {this.state.loading || !this.state.transaction ? 
                 (<div>loading...</div> ): 
                 (<div>
-                    {transactionsItems}
+
+                    <table>
+                        <thead>
+                            <th>Blockchain</th>
+                            <th>Quantity</th>
+                            <th>USD</th>
+                            <th>From</th>
+                            <th>To</th>
+                        </thead>
+                        <tbody>
+                            {this.state.transaction.map((transaction, i) => {
+                                return (
+                                    <tr key={i}>
+                                        <td>{transaction.blockchain}</td>
+                                        <td>{transaction.amount}</td>
+                                        <td>{transaction.amount_usd}</td>
+                                        <td>{transaction.from.owner}</td>
+                                        <td>{transaction.to.owner}</td>
+                                    </tr>
+                                )
+                            })}
+                        </tbody>
+                    </table>
                 </div>)}
             </div>
         );
