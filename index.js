c8f005d890907a713844da01996268a3b24e956d
ubryce <bknguyen@ualberta.ca>
2020-04-02 20:14:36 -0600

    deleting

--- index.js
+++ index.js
@@ -18,14 +18,6 @@ app.use('/bitcoin', bitcoinRoute);
 // Serve the static files from the React app
 app.use(express.static(path.join(__dirname, 'client/build')));
 
-/*
-// An api endpoint that returns a short list of items
-app.get('/api/getList', (req,res) => {
-	var list = ["item1", "item2", "item3"];
-	res.json(list);
-	console.log('Sent list of items');
-});
-*/
 
 //connect to database
 mongoose.connect(process.env.DB_CONNECTION, { useNewUrlParser: true }, ()=> 
