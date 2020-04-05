c7095daa9d70b40a742535f54dda76f59e97e595
ubryce <bknguyen@ualberta.ca>
2020-04-05 13:49:51 -0600

    ws client

--- client/src/App/pages/Bitcoin.js
+++ client/src/App/pages/Bitcoin.js
@@ -1,11 +1,26 @@
 import React, { Component } from 'react'
+import { w3cwebsocket as W3CWebSocket } from "websocket";
 
-function Bitcoin() {
+const client = new W3CWebSocket('ws://127.0.0.1:8000');
+
+class Bitcoin extends Component {
+
+    componentWillMount() {
+        client.onopen = () => {
+          console.log('WebSocket Client Connected');
+        };
+        client.onmessage = (message) => {
+          console.log(message);
+        };
+      }
+
+    render(){
     return (
         <div>
             <h1>Bitcoin</h1>
         </div>
     );
+}
 }
 
 export default Bitcoin;
